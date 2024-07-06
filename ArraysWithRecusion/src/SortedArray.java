ad5cf5bf5809d95134cf25696b7292d343d5a43c
tallminion <sagarkr1109@gmail.com>
2024-07-06 14:48:50 +0530

    check if array is sorted

---
+++ ArraysWithRecusion/src/SortedArray.java
@@ -0,0 +1,13 @@
+public class SortedArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 5, 9};
+        System.out.println(sorted(arr, 0));
+    }
+    static boolean sorted(int[] arr, int index){
+        if(index == arr.length-1){
+            return true;
+        }
+
+        return arr[index]<arr[index+1] && sorted(arr, index+1);
+    }
+}
